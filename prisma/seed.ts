2e3ca410ad4823d133f2eca19890807998b86030
Alba Vilanova Cortezon <alba.vilanova@outlook.com>
2024-04-28 19:17:25 +0200

    Format files

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -4,67 +4,67 @@ const db = new PrismaClient();
 
 // Add CustomWeather provider
 const CustomWeather = await db.provider.create({
-    data: {
-        name: "CustomWeather",
-        headquarters: "United States",
-        products: {
-            createMany: {
-                data: [
-                    {
-                        "name": "Ski Resort Weather Forecasts And Condition Reports",
-                        "coverage": ["Global"]
-                    },
-                    {
-                        "name": "Real-Time Earthquake Reports",
-                        "coverage": ["Global"]
-                    }
-                ]
-            }
-        }
-    }
-})
-console.log("Data from CustomWeather was added")
+  data: {
+    name: "CustomWeather",
+    headquarters: "United States",
+    products: {
+      createMany: {
+        data: [
+          {
+            name: "Ski Resort Weather Forecasts And Condition Reports",
+            coverage: ["Global"],
+          },
+          {
+            name: "Real-Time Earthquake Reports",
+            coverage: ["Global"],
+          },
+        ],
+      },
+    },
+  },
+});
+console.log("Data from CustomWeather was added");
 
 // Add Meteosource Weather provider
 const MeteosourceWeather = await db.provider.create({
-    data: {
-        name: "Meteosource Weather",
-        headquarters: "Czech Republic",
-        products: {
-            createMany: {
-                data: [
-                    {
-                        "name": "Solar power and radiation and wind power forecasts",
-                        "coverage": ["Global"]
-                    },
-                    {
-                        "name": "Air quality forecasts",
-                        "coverage": ["Global"]
-                    }
-                ]
-            }
-        }
-    }
-})
-console.log("Data from Meteosource Weather was added")
+  data: {
+    name: "Meteosource Weather",
+    headquarters: "Czech Republic",
+    products: {
+      createMany: {
+        data: [
+          {
+            name: "Solar power and radiation and wind power forecasts",
+            coverage: ["Global"],
+          },
+          {
+            name: "Air quality forecasts",
+            coverage: ["Global"],
+          },
+        ],
+      },
+    },
+  },
+});
+console.log("Data from Meteosource Weather was added");
 
 // Create users
 const users = await db.user.createMany({
-    data: [
-        {
-            "firstName": "Alba",
-            "lastName": "Vilanova",
-            "email": "alba.vilanova@gmail.com",
-            "organization": "BSC",
-            "position": "Research engineer",
-        },
-        {
-            "firstName": "David",
-            "lastName": "Gómez",
-            "email": "david.gomez@gmail.com",
-            "organization": "Kaya",
-            "position": "Web developer"
-        }
-    ]
-})
-console.log("Users were added")
+  data: [
+    {
+      firstName: "Alba",
+      lastName: "Vilanova",
+      organization: "BSC",
+      position: "Research engineer",
+      emails: ["alba.vilanova@gmail.com"],
+    },
+    {
+      firstName: "David",
+      lastName: "Gómez",
+      organization: "Kaya",
+      position: "Web developer",
+      emails: ["david.gomez@gmail.com"],
+    },
+  ],
+});
+console.log("Users were added");
